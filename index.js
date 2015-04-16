a8950fe6d87085aa6c8db58f73fdbd6945423f7e
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2015-04-16 14:39:31 -0300

    Habilitando plugin de ebook novamente

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var theme = require("./theme");
+var ebook = require("./ebook");
 
 module.exports = {
 	  ebook : theme.ebook
     , book : theme.book
     , templates: theme.templates
-    /*, hooks: {
-        "page": handlePage,
-        "page:after": handlePageAfter,
-        "ebook:before": handleEbookBefore
-    }*/
+    , hooks: {
+        "page": ebook.handlePage,
+        "page:after": ebook.handlePageAfter,
+        "ebook:before": ebook.handleEbookBefore
+    }
 }
