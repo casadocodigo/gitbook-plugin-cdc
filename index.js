02890cbc04474ad379bf237856e8c667a736641f
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2015-04-16 14:18:44 -0300

    Organizando plugins

---
+++ index.js
@@ -0,0 +1,12 @@
+var theme = require("./theme");
+
+module.exports = {
+	  ebook : theme.ebook
+    , book : theme.book
+    , templates: theme.templates
+    /*
+    , hooks: {
+    }
+    , hooks: {
+    }*/
+}
