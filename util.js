d61e5db52d48577cf190f7fb160c563717d7835a
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2015-04-24 16:53:32 -0300

    Adicionando obtainPdfOptions em util.js

--- util.js
+++ util.js
@@ -22,7 +22,35 @@ function obtainExtension(options) {
 	return extension;
 }
 
+function obtainPdfOptions(options) {
+    var pdfOptions = PDF_OPTIONS;
+    if (options.pdf) {
+        if (options.pdf.fontSize) {
+            pdfOptions["--pdf-default-font-size"] = ""+options.pdf.fontSize;
+            pdfOptions["--pdf-mono-font-size"] =  ""+options.pdf.fontSize;
+        }
+        if (options.pdf.customSize){
+            pdfOptions["--custom-size"] = ""+options.pdf.customSize;
+        }
+        if (options.pdf.margin) {
+            if (options.pdf.margin.top) {
+                pdfOptions["--margin-top"] =  ""+options.pdf.margin.top;
+            }
+            if (options.pdf.margin.bottom) {
+                pdfOptions["--margin-bottom"] =  ""+options.pdf.margin.bottom;
+            }
+            if (options.pdf.margin.left) {
+                pdfOptions["--margin-left"] =  ""+options.pdf.margin.left;
+            }
+            if (options.pdf.margin.right) {
+                pdfOptions["--margin-right"] =  ""+options.pdf.margin.right;
+            }
+        }
+    }
+    return pdfOptions;
+}
+
 module.exports = {
     "obtainExtension" : obtainExtension,
-    "pdfOptions": PDF_OPTIONS
+    "obtainPdfOptions": obtainPdfOptions
 }
