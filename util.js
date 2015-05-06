f5c22e2a7a4a7863eaab69152ab36fa2d3a3b33b
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2015-05-06 13:49:55 -0300

    Margens e tamanho da fonte configurados devem ser usados nos pré-conteúdos renderizados por fora do gitbook (sumário e introdução).

--- util.js
+++ util.js
@@ -6,8 +6,17 @@ var PDF_OPTIONS = {
     "--paper-size": null,
     "--custom-size": "155x230",
     "--unit": "millimeter",
+
     "--pdf-header-template": "<p id='ebook-header' style='border-bottom: 1px solid black; margin-top: 36pt;'><span class='odd_page'><span>{{PUBLISHER}}</span><span style='float:right'>_SECTION_</span></span><span class='even_page'><span>_SECTION_</span><span style='float:right'>{{PUBLISHER}}</span></span><script>if(!(/^[0-9]/.test('_SECTION_'))) { document.getElementById('ebook-header').style.display='none'; }</script></p>",
-    "--pdf-footer-template": "<p id='ebook-footer'></p><script>var footer = document.getElementById('ebook-footer'); footer.innerHTML = _PAGENUM_ - 2; if(_PAGENUM_ % 2 != 0){ footer.style.textAlign = 'right'; }</script>"
+    "--pdf-footer-template": "<p id='ebook-footer'></p><script>var footer = document.getElementById('ebook-footer'); footer.innerHTML = _PAGENUM_ - 2; if(_PAGENUM_ % 2 != 0){ footer.style.textAlign = 'right'; }</script>",
+
+    //valores padrão do gitbook
+    "--pdf-default-font-size": "12", 
+    "--pdf-mono-font-size": "12",
+    "--margin-left": "62",
+    "--margin-right": "62",
+    "--margin-top": "36",
+    "--margin-bottom": "36"
 };
 
 function obtainExtension(options) {
@@ -24,6 +33,17 @@ function obtainPdfOptions(options) {
         pdfOptions["--pdf-header-template"] = options.pdf.headerTemplate || pdfOptions["--pdf-header-template"].replace(/{{PUBLISHER}}/g, options.publisher || "Casa do Código");
         pdfOptions["--pdf-footer-template"] = options.pdf.footerTemplate || pdfOptions["--pdf-footer-template"];
         pdfOptions["--custom-size"] = options.pdf.customSize || pdfOptions["--custom-size"];
+        
+        pdfOptions["--pdf-default-font-size"] = ""+options.pdf.fontSize || pdfOptions["--pdf-default-font-size"];
+        pdfOptions["--pdf-mono-font-size"] =  ""+options.pdf.fontSize || pdfOptions["--pdf-mono-font-size"];
+        
+        if (options.pdf.margin) {
+             pdfOptions["--margin-top"] =  ""+options.pdf.margin.top || pdfOptions["--margin-top"];
+             pdfOptions["--margin-bottom"] =  ""+options.pdf.margin.bottom || pdfOptions["--margin-bottom"];
+             pdfOptions["--margin-left"] =  ""+options.pdf.margin.left || pdfOptions["--margin-left"];
+             pdfOptions["--margin-right"] =  ""+options.pdf.margin.right || pdfOptions["--margin-right"];
+        }
+        
     }
     return pdfOptions;
 }
