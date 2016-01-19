ab49d53a3efb8dc113f15e7771dd9e7a0208ea0e
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2016-01-19 14:26:45 -0200

    partes

--- ebook/index.js
+++ ebook/index.js
@@ -14,6 +14,8 @@ var WIDTH_REGEX = /\{w=(\d+)%?\}$/;
 
 var DESKTOP_WIDTH = 1000;
 
+var parts = {};
+
 module.exports = {
     "handlePageBefore": handlePageBefore,
     "handlePage": handlePage,
@@ -25,7 +27,7 @@ function handlePageBefore(page) {
     var maxLength = this.options.maxLineLength || 80;
     var filename = page.path == "README.md" ? this.options.firstChapter+".md" : page.path;
     var dentroDeCode = false;
-    
+
     //nao foi utilizada regex para manter numero de linha (i)
     page.content.split("\n").forEach(function(line, i){
         if(!dentroDeCode && line.trim().indexOf("```") == 0) {
@@ -38,7 +40,7 @@ function handlePageBefore(page) {
         }
 
         if(dentroDeCode && line.length > maxLength){
-            console.log('warning: code "'+line.trim().substring(0, 30) + '"... too long. was ' + line.length + ' (max ' + maxLength + ') in ' + filename+':'+(i+1));		
+            console.log('warning: code "'+line.trim().substring(0, 30) + '"... too long. was ' + line.length + ' (max ' + maxLength + ') in ' + filename+':'+(i+1));
         }
     });
     return page;
@@ -64,10 +66,42 @@ function handlePage(page) {
 
 function handlePageAfter(page) {
     var options = this.options;
+    var extension = util.obtainExtension(options);
 
     renderIntro(options);
 
     var chapter = page.progress.current;
+
+    var chapterPath = chapter.path == "README.md" ? options.firstChapter+".md" : chapter.path;
+    var chapterDir = path.dirname(chapterPath);
+    if(chapterDir.indexOf("part-") == 0){
+        var partHeaderPath = options.partHeaders.filter(function(partHeader){
+            return path.dirname(partHeader) == chapterDir;
+        })[0];
+
+        if(partHeaderPath && !parts[partHeaderPath]) {
+            parts[partHeaderPath] = true;
+
+            var partHeaderFile = path.join(options.input, partHeaderPath);
+            var partHeaderMd = fs.readFileSync(partHeaderFile);
+            var partHeaderHtml = kramed(partHeaderMd.toString());
+
+            var $ = cheerio.load(partHeaderHtml);
+            var img = $("img");
+            if(chapter.path == "README.md"){
+                var imgSrc = img.attr("src");
+                imgSrc = imgSrc.replace(/^\.\.\//, "");
+                img.attr("src", imgSrc);
+            }
+            adjustImageWidth(img, extension);
+            partHeaderHtml = $.html();
+
+            var partHeader = '<div class="part-header">\n' + partHeaderHtml + '</div>\n';
+            var $ = cheerio.load(page.content);
+            $(".page").prepend(partHeader);
+            page.content = $.html();
+        }
+    }
 
     if(isIntroFile(chapter, options)){
        return page;
@@ -92,6 +126,7 @@ function handlePageAfter(page) {
 
 
 function handleEbookBefore(options) {
+
     var extension = util.obtainExtension(this.options);
 
     //options["-d debug"] = true;
@@ -149,10 +184,10 @@ function renderIntro(options){
 
 
 function verifyChapterTitle(chapter){
-	var chapterTitle = chapter.title;
-	if(/^[0-9]/.test(chapterTitle)){
-		throw new Error("Chapter can't begin with numbers: " + chapterTitle);
-	}
+    var chapterTitle = chapter.title;
+    if(/^[0-9]/.test(chapterTitle)){
+        throw new Error("Chapter can't begin with numbers: " + chapterTitle);
+    }
 }
 
 function obtainChapterNumber(chapter, options) {
@@ -194,38 +229,45 @@ function sectionNumber(chapterNumber, text, i) {
 
 function adjustImages($, chapter, section, options) {
     var extension = util.obtainExtension(options);
-
     var chapterNumber = obtainChapterNumber(chapter, options);
     $("img").each(function (i) {
         var img = $(this);
-        var text = img.attr("alt").trim();
-
-        //ajusta width da imagem
-        var regexMatch = text.match(WIDTH_REGEX);
-        if (regexMatch && regexMatch[1]) {
-            text = text.replace(WIDTH_REGEX, "");
-            var width = regexMatch[1];
-            if(extension == "pdf") {
-                img.css("width", width + "%");
-            } else {
-                var maxWidth = parseInt(width)/100 * DESKTOP_WIDTH;
-                img.css("max-width", maxWidth + "px");
-            }
-        }
-
-        //insere div com caption
-        var parent = img.parent();
-        img.attr("alt", text).remove();
-        var figure = $("<div class='figure'>").append(img);
-        parent.after(figure);
-        if(text.trim().length > 0){
-            var caption = $("<p class='figcaption'>");
-            caption.text(CAPTION_PREFIX + chapterNumber + "." + (i + 1) + ": " + text.trim());
-            figure.append(caption);
-        }
+        adjustImageWidth(img, extension);
+        insertImageCaption($, img, i, chapterNumber);
     });
 
     section.content = $.html();
+}
+
+function adjustImageWidth(img, extension){
+    //ajusta width da imagem
+    var text = img.attr("alt").trim();
+    var regexMatch = text.match(WIDTH_REGEX);
+    if (regexMatch && regexMatch[1]) {
+        text = text.replace(WIDTH_REGEX, "");
+        var width = regexMatch[1];
+        if(extension == "pdf") {
+            img.css("width", width + "%");
+        } else {
+            var maxWidth = parseInt(width)/100 * DESKTOP_WIDTH;
+            img.css("max-width", maxWidth + "px");
+        }
+        img.attr("alt", text);
+    }
+}
+
+function insertImageCaption($, img, i, chapterNumber){
+    //insere div com caption
+    var text = img.attr("alt").trim();
+    var parent = img.parent();
+    img.remove();
+    var figure = $("<div class='figure'>").append(img);
+    parent.after(figure);
+    if(text.length > 0){
+        var caption = $("<p class='figcaption'>");
+        caption.text(CAPTION_PREFIX + chapterNumber + "." + (i + 1) + ": " + text);
+        figure.append(caption);
+    }
 }
 
 function removeComments($, section){
